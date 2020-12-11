efa7a3696b9666766954c962eb33cb7827ce8c5d
hcykg2 <hcykg2@nottingham.edu.my>
2020-12-12 01:05:06 +0800

    added turtle animation

--- src/model/Turtle.java
+++ src/model/Turtle.java
@@ -1,15 +1,36 @@
 package model;
 
+import java.util.ArrayList;
+
 import controller.BoundsController;
+import javafx.animation.AnimationTimer;
 import javafx.scene.image.Image;
 
 public class Turtle extends Actor {
-	Image turtle1;
-	Image turtle2;
-	Image turtle3;
+	ArrayList<Image> sprites = new ArrayList<Image>();
 	private int speed;
 	int i = 1;
 	boolean bool = true;
+	
+	AnimationTimer animation = new AnimationTimer() {
+		int time = 120;
+		int counter = 0;
+		int sprite = 0;
+		@Override
+		public void handle(long arg0) {
+			if (counter < time) {
+				counter++;
+			} else {
+				counter = 0;
+				sprite++;
+				if (sprite == sprites.size()) {
+					sprite = 0;
+				}
+				setImage(sprites.get(sprite));
+			}
+		}
+	};
+	
 	@Override
 	public void act(long now) {		
 		move(speed , 0);
@@ -22,11 +43,14 @@ public class Turtle extends Actor {
 		}
 	}
 	public Turtle(int x, int y, int s) {
-		turtle1 = new Image("file:src/assets/turtle_0.png", World.getGridSize(), World.getGridSize(), true, true);
+		sprites.add(new Image("file:src/assets/turtle_0.png", World.getGridSize(), World.getGridSize(), true, true));
+		sprites.add(new Image("file:src/assets/turtle_1.png", World.getGridSize(), World.getGridSize(), true, true));
+		sprites.add(new Image("file:src/assets/turtle_2.png", World.getGridSize(), World.getGridSize(), true, true));
 		setGridX(x);
 		setGridY(y);
 		speed = s;
-		setImage(turtle1);
+		setImage(sprites.get(0));
+		animation.start();
 	}
 	
 	public double getSpeed() {
